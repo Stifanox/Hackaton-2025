1a870f1524c439189917d8274b70d7e0491a6de9
Micreg102 <mregiew@gmail.com>
2025-05-31 19:38:14 +0200

    [Frontend] grid paneli

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Coordinates from "./pages/coordinates/Coordinates";
 
+
+import AreaVisualizer from "./components/AreaVisualizer";
 function App() {
   return (
     <div>
@@ -10,6 +12,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
             <Route path="/coordinates" element={<Coordinates />} />
+            <Route
+                path="/area" // <-- Nowa ścieżka
+                element={<AreaVisualizer/>}
+                />
         </Routes>
       </Router>
     </div>
