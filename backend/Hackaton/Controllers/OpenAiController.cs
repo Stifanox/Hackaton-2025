42818731fc2f9c9de1df33aadd968b4fe5cfba0a
Mateusz <mateusz.fudzsl@hotmail.com>
2025-06-01 08:52:40 +0200

    [Backend] OpenAI: Add open ai service

---
+++ backend/Hackaton/Controllers/OpenAiController.cs
@@ -0,0 +1,30 @@
+using Hackaton.DTOs;
+using Hackaton.Helpers;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackaton.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OpenAiController: ControllerBase
+{
+    private readonly OpenAiService _openAiService;
+
+    public OpenAiController(OpenAiService openAiService)
+    {
+        _openAiService = openAiService;
+    }
+
+    [HttpPost("get-response")]
+    public async Task<IActionResult> OpenAi([FromBody] MessageInput message)
+    {
+        if (ModelState.IsValid)
+        {
+            return BadRequest(new ErrorResponse<string>(data:"Message not provided"));
+        }
+        var result = await _openAiService.AskChatAsync("test");
+
+        return Ok(new SuccessResponse<string>(data:result));
+    }
+}
